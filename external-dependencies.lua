e6973f4b30eff4e7e7e27e0fed0e84b98de4374f
thetaepsilon <thetaepsilon@noreply.users.github.com>
2017-10-09 23:13:03 +0100

    logger.lua: implement appender set forwarding using modhelpers set datastruct

--- external-dependencies.lua
+++ external-dependencies.lua
@@ -1,2 +1,2 @@
 _log.shallowcopy = modhelpers.readonly.shallowcopy
-
+_log.mkset = modhelpers.datastructs.new.set
--- logger.lua
+++ logger.lua
@@ -2,6 +2,9 @@
 -- acts as a multiplexer to all attached appenders.
 -- events will be forwarded to each in turn.
 -- may itself also act as an appender for another logger.
+-- call ancestor_logger.forward() to retrieve an appropriate appender closure function.
+
+local mkset = _log.mkset
 
 local construct = function(opts)
 	local typecheck = type(opts)
@@ -13,12 +16,28 @@ local construct = function(opts)
 		error("options argument for logger constructor expected to be a table or nil, got "..typecheck)
 	end
 
+	local name = opts.name
+	if type(name) ~= "string" then name = "" end
 	-- no private variable access for you!
 	local self = {
-		-- I miss hashmaps...
-		appenders = {}
+		appenders = mkset()
 	}
+	local dolog = function(event)
+		for appender in self.appenders.iterator() do appender(event) end
+	end
 	return {
-		
+		name = function() return name end
+		appender_add = function(appender)
+			if type(appender) ~= "function" then
+				error("appender must be a function!")
+			end
+			return self.appenders.add(appender)
+		end,
+		-- oh, I love closures...
+		appender_remove = self.appenders.remove,
+		log = dolog,
+		forward = function() return dolog end
 	}
 end
+
+return construct
